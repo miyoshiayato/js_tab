7771a25ad30e75ef02c52a2818bbf5f178a6ceb4
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-02-13 23:47:51 +0900

    後で変数名を変えやすいように追記

--- tab.js
+++ tab.js
@@ -4,6 +4,7 @@
   const $tab = $doc.getElementById('js-tab');
   const $nav = $tab.querySelectorAll('[data-nav]');
   const $content = $tab.querySelectorAll('[data-content]');
+  const ACTIVE_CLASS = 'is-active';
 
   //初期化
   const init = () => {
