deb6720cb0b6f59f2e6528bfaa0240f9a856a876
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-02-13 23:29:26 +0900

    一つ目のタブだけdisplay: blockになるよう記述

--- tab.js
+++ tab.js
@@ -4,4 +4,11 @@
   const $tab = $doc.getElementById('js-tab');
   const $nav = $tab.querySelectorAll('[data-nav]');
   const $content = $tab.querySelectorAll('[data-content]');
+
+  //初期化
+  const init = () => {
+    $content[0].style.display = 'block';
+  };
+  init();
+  
   };();
